8be62eac258a30898022ee3c7a8b75faf9661a19
Eduardo Díaz <183442@ids.upchiapas.edu.mx>
2021-11-23 07:56:47 -0600

    env

--- helpers/init_mongodb.js
+++ helpers/init_mongodb.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const DB = mychat-db;
-const DBPASSWORD = chat123view;
-const DBUSER = chatview
+const DB = "mychat-db";
+const DBPASSWORD = "chat123view";
+const DBUSER ="chatview"
 
 const mongoDB = `mongodb+srv://${DBUSER}:${DBPASSWORD}@messages.hbzua.mongodb.net/${DB}?retryWrites=true&w=majority`
 
