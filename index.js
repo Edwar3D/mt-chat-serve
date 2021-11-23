691331a394589df57d2c34e1946438581ecf3ab4
Eduardo Díaz <183442@ids.upchiapas.edu.mx>
2021-11-23 11:07:48 -0600

    env

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.set('port', 3000);
 
-const http = require('https');
+const http = require('http');
 
 var server = http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
